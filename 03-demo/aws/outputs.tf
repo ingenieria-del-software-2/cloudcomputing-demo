4f81d4a8edcb1fc197d65bf536ab1944603867c6
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2025-04-14 03:35:21 -0300

    feat(aws): add AWS credentials and EC2 connection outputs to Terraform configuration, enhancing security and usability

--- 03-demo/aws/outputs.tf
+++ 03-demo/aws/outputs.tf
@@ -1,18 +1,38 @@
-# Output: EC2 instance ID
-output "instance_id" {
-  value = aws_instance.docker_instance.id
+# Pipeline credentials
+output "AWS_ACCESS_KEY_ID" {
+  value       = aws_iam_access_key.pipeline_user_key.id
+  description = "Access key del usuario IAM `pipeline-user`"
+  sensitive   = true
 }
 
-# Output: EC2 instance public IP
-output "public_ip" {
-  value = aws_instance.docker_instance.public_ip
+output "AWS_SECRET_ACCESS_KEY" {
+  value       = aws_iam_access_key.pipeline_user_key.secret
+  description = "Secret key del usuario IAM `pipeline-user`"
+  sensitive   = true
+}
+
+output "AWS_ACCOUNT_ID" {
+  value       = data.aws_caller_identity.current.account_id
+  description = "ID numérico de tu cuenta AWS (sin espacios ni guiones)"
+}
+
+# EC2 connection details
+output "EC2_HOST" {
+  value       = aws_instance.docker_instance.public_ip
+  description = "Dirección IP público de la instancia EC2"
+}
+
+# The EC2 SSH key needs to be generated separately and provided as input
+output "EC2_SSH_KEY" {
+  value       = "La clave privada del Key Pair está en tu sistema local"
+  description = "Clave privada del Key Pair utilizada por EC2 (formato texto plano, sin passphrase)"
 }
 
 # Output: EC2 instance public DNS
-output "public_dns" {
+output "EC2_PUBLIC_DNS" {
   value = aws_instance.docker_instance.public_dns
 }
 
-output "ecr_repository_url" {
+output "ECR_REPOSITORY_URL" {
   value = aws_ecr_repository.microservice_repo.repository_url
 }
